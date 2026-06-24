3a1b299099493f81cf868358314678ad81fa0a5f
Salman Khan <salmantechhub336@gmail.com>
2026-06-24 21:57:17 +0500

    changes

---
+++ src/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+async function connectDB(){
+    try {
+        
+    } catch (error) {
+        console.log("database connection failed", error);
+        process.exit(1);
+        
+    }
+}
