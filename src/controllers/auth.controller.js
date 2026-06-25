49eb7c257f046cb53e46cad463c63c16bee71453
Salman Khan <salmantechhub336@gmail.com>
2026-06-25 14:21:41 +0500

    login api done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,53 +3,95 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 async function registerUser(req, res) {
-  const { username, email, password, role = "user" } = req.body;
+    const { username, email, password, role = "user" } = req.body;
 
-  const isUserAlreadyExist = await userModel.findOne({
-    $or: [{ username }, { email }],
-  });
-
-  if (isUserAlreadyExist) {
-    return res.status(409).json({
-      message: "user already exist",
+    const isUserAlreadyExist = await userModel.findOne({
+        $or: [{ username }, { email }],
     });
-  }
 
-  const hash = await bcrypt.hash(password, 10)
+    if (isUserAlreadyExist) {
+        return res.status(409).json({
+            message: "user already exist",
+        });
+    }
 
-  const user = await userModel.create({
-    username,
-    email,
-    password: hash,
-    role,
-  });
+    const hash = await bcrypt.hash(password, 10)
 
-  const token = jwt.sign(
-    {
-      id: user._id,
-      role: user.role,
-    },
-    process.env.JWT_SECRET,
-  );
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash,
+        role,
+    });
 
-  res.cookie("token", token);
+    const token = jwt.sign(
+        {
+            id: user._id,
+            role: user.role,
+        },
+        process.env.JWT_SECRET,
+    );
 
-  res.status(201).json({
-    message: "user created sucessfully",
+    res.cookie("token", token);
 
-    user: {
-      id: user._id,
-      username: user.username,
-      email: user.email,
-      role: user.role,
-    },
-  });
+    res.status(201).json({
+        message: "user created sucessfully",
+
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role,
+        },
+    });
 }
 
-async function loginUser(req, res){
-    const {username, email, password} = req.body;
-    
+async function loginUser(req, res) {
+    const { username, email, password } = req.body;
+
+
+    const user = await userModel.findOne({
+        $or: [
+            { username },
+            { email }
+        ]
+
+    })
+
+    if (!user) {
+        return res.status(401).json({
+            message: "invalid credentials"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid){
+        return res.status(401).json({
+            message:"inavlid credentials"
+        })
+    }
+
+    const token = jwt.sign({
+        id:user._id,
+        role:user.role
+    },
+    process.env.JWT_SECRET
+)
+
+res.cookie("token", token)
+
+res.status(200).json({
+    message:"user logged in sucessfully",
+    user:{
+        id:user._id,
+        username:user.username,
+        email:user.email,
+        role:user.role
+    }
+})
+
 }
 
 
-module.exports = {registerUser};
+module.exports = { registerUser };
